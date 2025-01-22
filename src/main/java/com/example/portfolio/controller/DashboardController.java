4a0396c3b5528b4b25b6e5c248d710022faecf19
Aniket Shelar <150237726+AniketShelar22@users.noreply.github.com>
2025-01-22 12:41:03 +0530

    Update DashboardController.java

--- src/main/java/com/example/portfolio/controller/DashboardController.java
+++ src/main/java/com/example/portfolio/controller/DashboardController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins ="https://capx-portfolio-tracker.netlify.app/")
+@CrossOrigin(origins ="https://capx-portfolio-tracker.netlify.app")
 
 @RestController
 public class DashboardController {
